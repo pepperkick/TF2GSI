79eed2222ec3bbc0cffee54fce57eaa13e1550ed
PepperKick <abhishrant.m@gmail.com>
2019-03-11 22:59:08 +0530

    init

---
+++ src/main.cpp
@@ -0,0 +1,102 @@
+// Fix INVALID_HANDLE_VALUE redefinition warning
+#ifndef _LINUX
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifndef NULL
+#define NULL nullptr
+#endif
+
+#include "interface.h"
+#include "filesystem.h"
+#include "engine/iserverplugin.h"
+#include "game/server/iplayerinfo.h"
+#include "eiface.h"
+#include "igameevents.h"
+#include "convar.h"
+#include "Color.h"
+#include "vstdlib/random.h"
+#include "engine/IEngineTrace.h"
+#include "tier2/tier2.h"
+#include "ihltv.h"
+#include "ihltvdirector.h"
+#include "KeyValues.h"
+#include "dt_send.h"
+#include "server_class.h"
+
+#include <vector>
+#include <set>
+#include <string>
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+#define PRINT_TAG() ConColorMsg(Color(0, 153, 153, 255), "[GSI] ")
+#define PLUGIN_VERSION "0.0.1"
+
+IServerGameDLL* g_pGameDLL;
+IFileSystem* g_pFileSystem;
+IHLTVDirector* g_pHLTVDirector;
+IVEngineServer* engine;
+
+class Plugin : public IServerPluginCallbacks {
+public:
+  virtual bool Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory) override;
+  virtual void Unload() override { }
+  virtual void Pause() override { }
+  virtual void UnPause() override { }
+  virtual const char* GetPluginDescription() override { return "GSI"; }
+  virtual void LevelInit(const char* mapName) override { }
+  virtual void ServerActivate(edict_t* pEdictList, int edictCount, int clientMax) override { }
+  virtual void GameFrame(bool simulating) override { }
+  virtual void LevelShutdown() override { }
+  virtual void ClientActive(edict_t* pEntity) override { }
+  virtual void ClientDisconnect(edict_t* pEntity) override { }
+  virtual void ClientPutInServer(edict_t* pEntity, const char* playername) override;
+  virtual void SetCommandClient(int index) override { }
+  virtual void ClientSettingsChanged(edict_t* pEdict) override { }
+  virtual PLUGIN_RESULT ClientConnect(bool* bAllowConnect, edict_t* pEntity, const char* pszName, const char* pszAddress, char* reject, int maxrejectlen) override { return PLUGIN_CONTINUE; }
+  virtual PLUGIN_RESULT ClientCommand(edict_t* pEntity, const CCommand& args) override { return PLUGIN_CONTINUE; }
+  virtual PLUGIN_RESULT NetworkIDValidated(const char* pszUserName, const char* pszNetworkID) override { return PLUGIN_CONTINUE; }
+  virtual void OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t* pPlayerEntity, EQueryCvarValueStatus eStatus, const char* pCvarName, const char* pCvarValue) override { }
+  virtual void OnEdictAllocated(edict_t* edict) { }
+  virtual void OnEdictFreed(const edict_t* edict) { }
+};
+
+Plugin g_Plugin;
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(Plugin, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_Plugin);
+
+bool Plugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory) {
+  PRINT_TAG();
+  ConColorMsg(Color(255, 255, 0, 255), "Loading plugin...\n");
+
+  g_pGameDLL = (IServerGameDLL*)gameServerFactory(INTERFACEVERSION_SERVERGAMEDLL, nullptr);
+  if(!g_pGameDLL) {
+    PRINT_TAG();
+    ConColorMsg(Color(255, 0, 0, 255), "Could not find game DLL interface, aborting load\n");
+    return false;
+  }
+
+  engine = (IVEngineServer*)interfaceFactory(INTERFACEVERSION_VENGINESERVER, nullptr);
+  if (!engine) {
+    PRINT_TAG();
+    ConColorMsg(Color(255, 0, 0, 255), "Could not find engine interface, aborting load\n");
+    return false;
+  }
+
+  g_pFileSystem = (IFileSystem*)interfaceFactory(FILESYSTEM_INTERFACE_VERSION, nullptr);
+  if(!g_pFileSystem) {
+    Error("[GSI] Could not find filesystem interface, aborting load\n");
+    return false;
+  }
+
+  PRINT_TAG();
+  ConColorMsg(Color(255, 255, 0, 255), "Successfully Started, Version: %s\n", PLUGIN_VERSION);
+  return true;
+}
+
+void Plugin::ClientPutInServer(edict_t *pEntity, char const *playername) {
+    PRINT_TAG();
+    ConColorMsg(Color(255, 255, 255, 255), "Client Joined: %s", playername);
+}
